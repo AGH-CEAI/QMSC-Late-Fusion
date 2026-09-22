a7c9c77f7e2e3e77357b5629f864858d37617f9f
sdziura <sabek12@gmail.com>
2026-09-22 20:18:11 +0200

    feat: update experiment

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import yaml
 import utils.mlflow as mf
+
+from scripts.test import test_reservoir
 from scripts.experiments.reservoir.initial_experiments import (
     initial_1,
     initial_classical_1,
+    initial_2_amplitude,
 )
 
 
@@ -10,9 +13,10 @@ def main():
     with open("config/config.yaml", "r") as f:
         config = yaml.safe_load(f)
     mf.setup_mlflow("QMSC_Late_Fusion")
-    # initial_1(config)
-    config["training"]["max_iter"] = 500
-    initial_classical_1(config)
+    config["training"]["max_iter"] = 100
+    initial_1(config.copy())
+    initial_classical_1(config.copy())
+    initial_2_amplitude(config.copy())
 
 
 if __name__ == "__main__":
--- scripts/test.py
+++ scripts/test.py
@@ -1,4 +1,4 @@
-from models.extractors.reservoir import QuantumReservoir
+from models.extractors.reservoir import QuantumReservoir, QuantumReservoir2
 from qiskit.circuit.random import random_circuit
 from qiskit.circuit.library import z_feature_map
 import numpy as np
@@ -6,11 +6,11 @@ import numpy as np
 
 def test_reservoir():
     encoder = z_feature_map(feature_dimension=4)
-    qc = random_circuit(num_qubits=4, depth=5, measure=False, seed=42)
+    qc = random_circuit(num_qubits=3, depth=5, measure=False, seed=42)
 
-    reservoir = QuantumReservoir(
+    reservoir = QuantumReservoir2(
         encoding_qc=encoder,
         reservoir_qc=qc,
     )
-
-    return reservoir.extract_features(np.array([0.34, 0.5, 0.12, 0.9]))
+    res = reservoir._extract_features_single(np.array([0.34, 0.5, 0.12, 0.9]))
+    return res
