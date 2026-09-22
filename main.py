f4b36396e969a72dd8e2681654eb7046ae1247f6
sdziura <sabek12@gmail.com>
2026-09-22 17:27:16 +0200

    refac: update initial experiments to use config

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import yaml
-import data.loaders.hidden_manifold as dt
-from scripts.test import test_reservoir
+import utils.mlflow as mf
 from scripts.experiments.reservoir.initial_experiments import (
     initial_1,
     initial_classical_1,
-    initial_2,
-    initial_classical_2,
 )
 
 
 def main():
-    initial_2()
-    initial_classical_2()
+    with open("config/config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    mf.setup_mlflow("QMSC_Late_Fusion")
+    # initial_1(config)
+    config["training"]["max_iter"] = 500
+    initial_classical_1(config)
 
 
 if __name__ == "__main__":
--- scripts/experiments/reservoir/initial_experiments.py
+++ scripts/experiments/reservoir/initial_experiments.py
@@ -7,36 +7,30 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
 
 
-def initial_1(seed: int = 42):
+def initial_1(config: dict):
     # Load hidden-manifold data
-    with open("config/config.yaml", "r") as f:
-        config = yaml.safe_load(f)
     x_train, y_train, x_test, y_test = dt.get_manifold(
-        config["datasets"]["hidden-manifold"], dim=8, diff=False
+        **config["datasets"]["hidden-manifold"]
     )
 
     # Quantum Extreme Reservoi Computing model
-    encoder = z_feature_map(feature_dimension=8)
-    qc = random_circuit(num_qubits=8, depth=1, measure=False, seed=seed)
+    encoder = z_feature_map(
+        feature_dimension=config["datasets"]["hidden-manifold"]["dim"]
+    )
+    qc = random_circuit(**config["feature_extractor"]["random_circuit"])
     reservoir = QuantumReservoir(
         encoding_qc=encoder,
         reservoir_qc=qc,
     )
 
     # Classical Classifier
-    clf = MLPClassifier(
-        solver="sgd",
-        alpha=1e-5,
-        hidden_layer_sizes=(),
-        random_state=42,
-        max_iter=300,
-    )
+    clf = MLPClassifier(**config["training"])
 
     # Feature Extraction:
     train_features = reservoir.extract_features_batch(x_train)
     test_features = reservoir.extract_features_batch(x_test)
 
-    print(test_features[0])
+    # print(test_features[0])
 
     # Fit output layer
     clf.fit(train_features, y_train)
@@ -50,93 +44,14 @@ def initial_1(seed: int = 42):
     print(conf)
 
 
-def initial_classical_1(seed: int = 42):
+def initial_classical_1(config):
     # Load hidden-manifold data
-    with open("config/config.yaml", "r") as f:
-        config = yaml.safe_load(f)
     x_train, y_train, x_test, y_test = dt.get_manifold(
-        config["datasets"]["hidden-manifold"], dim=8, diff=False
+        **config["datasets"]["hidden-manifold"]
     )
 
     # Classical Classifier
-    clf = MLPClassifier(
-        solver="sgd",
-        alpha=1e-5,
-        hidden_layer_sizes=(),
-        random_state=42,
-        max_iter=300,
-    )
-
-    # Fit output layer
-    clf.fit(x_train, y_train)
-
-    mean_acc = clf.score(x_test, y_test)
-
-    print(mean_acc)
-
-
-##################################################################
-
-
-def initial_2(seed: int = 42):
-    # Load hidden-manifold data
-    with open("config/config.yaml", "r") as f:
-        config = yaml.safe_load(f)
-    x_train, y_train, x_test, y_test = dt.get_manifold(
-        config["datasets"]["hidden-manifold"], dim=4, diff=False
-    )
-
-    # Quantum Extreme Reservoi Computing model
-    encoder = z_feature_map(feature_dimension=4)
-    qc = random_circuit(num_qubits=4, depth=3, measure=False, seed=seed)
-    reservoir = QuantumReservoir(
-        encoding_qc=encoder,
-        reservoir_qc=qc,
-    )
-
-    # Classical Classifier
-    clf = MLPClassifier(
-        solver="sgd",
-        alpha=1e-5,
-        hidden_layer_sizes=(),
-        random_state=42,
-        max_iter=300,
-    )
-
-    # Feature Extraction:
-    train_features = reservoir.extract_features_batch(x_train)
-    test_features = reservoir.extract_features_batch(x_test)
-
-    print(test_features[0])
-
-    # Fit output layer
-    clf.fit(train_features, y_train)
-
-    y_pred = clf.predict(test_features)
-
-    mean_acc = clf.score(test_features, y_test)
-    print(mean_acc)
-
-    conf = confusion_matrix(y_test, y_pred=y_pred)
-    print(conf)
-
-
-def initial_classical_2(seed: int = 42):
-    # Load hidden-manifold data
-    with open("config/config.yaml", "r") as f:
-        config = yaml.safe_load(f)
-    x_train, y_train, x_test, y_test = dt.get_manifold(
-        config["datasets"]["hidden-manifold"], dim=4, diff=False
-    )
-
-    # Classical Classifier
-    clf = MLPClassifier(
-        solver="sgd",
-        alpha=1e-5,
-        hidden_layer_sizes=(),
-        random_state=42,
-        max_iter=300,
-    )
+    clf = MLPClassifier(**config["training"])
 
     # Fit output layer
     clf.fit(x_train, y_train)
