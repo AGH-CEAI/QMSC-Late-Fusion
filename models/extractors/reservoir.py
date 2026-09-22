15c21b88b1fb19cac12246a819f444549e898158
sdziura <sabek12@gmail.com>
2026-09-22 15:42:34 +0200

    feat: add extract_features_batch method to QuantumReservoir

--- models/extractors/reservoir.py
+++ models/extractors/reservoir.py
@@ -1,6 +1,7 @@
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
 import numpy.typing as npt
+import numpy as np
 
 
 class QuantumReservoir:
@@ -11,7 +12,12 @@ class QuantumReservoir:
     ):
         self.circuit = encoding_qc.compose(reservoir_qc)
 
-    def extract_features(self, x: npt.NDArray) -> npt.NDArray:
+    def _extract_features_single(self, x: npt.NDArray) -> npt.NDArray:
         bound_qc = self.circuit.assign_parameters(x)
         state = Statevector.from_instruction(bound_qc)
         return state.probabilities()
+
+    def extract_features_batch(self, x: npt.NDArray) -> npt.NDArray:
+        return np.array(
+            [self._extract_features_single(sample) for sample in x]
+        )
